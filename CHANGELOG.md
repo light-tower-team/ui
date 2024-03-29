6ba25157b5c2a2165ad6e529cd9155f6c15ae235
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-03-29 16:23:42 +0300

    chore(linter): update linter config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,5 @@
 # 1.0.0 (2024-03-28)
 
-
 ### Features
 
-* **components:** add button, icon, loading and tooltip components ([c6479df](https://github.com/light-tower-team/ui/commit/c6479dfc5b12d6bdfa4592e789f29f4212ff7eed))
+- **components:** add button, icon, loading and tooltip components ([c6479df](https://github.com/light-tower-team/ui/commit/c6479dfc5b12d6bdfa4592e789f29f4212ff7eed))
--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,3 @@
 import eslintPlugin from "@light-tower-team/eslint-plugin";
 
-const namingConventionRuleName = "@typescript-eslint/naming-convention";
-const namingConventionRules = eslintPlugin.configs.default.find((config) => config.rules?.[namingConventionRuleName])
-  ?.rules[namingConventionRuleName];
-
-export default [
-  ...eslintPlugin.configs.default,
-  {
-    rules: {
-      [namingConventionRuleName]: [
-        ...namingConventionRules.filter(
-          (rule) => !rule.selector?.includes("typeLike") || !rule.format?.includes("PascalCase"),
-        ),
-        {
-          selector: ["enumMember"],
-          format: ["PascalCase"],
-        },
-      ],
-    },
-  },
-];
+export default [...eslintPlugin.configs.default];
